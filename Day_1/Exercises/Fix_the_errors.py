d73033308d77588017dadad7b8648b49047ea60c
Rubaiat <iamcrubaiat@gmail.com>
2023-09-22 22:18:32 +0800

    Improve the code for line 16

--- Day_1/Exercises/Fix_the_errors.py
+++ Day_1/Exercises/Fix_the_errors.py
@@ -13,6 +13,6 @@ print(("New lines can be created with a backslash and n.")
 
 # Fixed code
 print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "'"+"'" sign.")
+print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
